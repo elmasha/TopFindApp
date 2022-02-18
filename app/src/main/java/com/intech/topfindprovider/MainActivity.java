e4cd186363c7da4f7003c40b5cd268c8bde42b40
user <elmashavitran@gmail.com>
2022-02-18 15:15:34 +0300

    28th

--- app/src/main/java/com/intech/topfindprovider/MainActivity.java
+++ app/src/main/java/com/intech/topfindprovider/MainActivity.java
@@ -105,9 +105,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
-
         NextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
